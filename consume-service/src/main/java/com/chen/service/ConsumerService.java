716a3cbd48e7a933e4acfdf13e236be8896fd1e1
chenjie <295477887@qq.com>
2018-07-18 18:25:45 +0800

    hystrix

--- consume-service/src/main/java/com/chen/service/ConsumerService.java
+++ consume-service/src/main/java/com/chen/service/ConsumerService.java
@@ -1,5 +1,8 @@
 package com.chen.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -10,10 +13,20 @@ import org.springframework.web.client.RestTemplate;
  */
 @Service
 public class ConsumerService {
+    Logger logger = LoggerFactory.getLogger(ConsumerService.class);
+
     @Autowired
     private RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "fallback")
     public String consumeLocation(){
         return restTemplate.getForObject("http://location-service/location", String.class);
     }
+
+    public String fallback(){
+        logger.info("fallback");
+        return "server is down!";
+    }
+
+
 }
